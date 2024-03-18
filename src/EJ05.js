a8135966c684a763fca4ed6370a547f0117309e2
EMILIA INÉS ARMENO <47697893@ort>
2024-03-18 11:53:02 -0300

    hola

--- src/EJ05.js
+++ src/EJ05.js
@@ -1,4 +1,4 @@
-import url from'./models/url.js';
+import url from'./modules/url.js';
 
 let objeto = parsearUrl("http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo");
 console.log(objeto);
